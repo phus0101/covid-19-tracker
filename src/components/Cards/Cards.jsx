4611c289e9cf280290fc2943451f282d039182e9
phus0101 <hothanhphu2k@gmail.com>
2020-04-07 12:01:27 +0700

    fix title in card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -18,7 +18,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               <Countup start={0} end={confirmed.value} duration={2.5} separator="," />
             </Typography>
             <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
@@ -28,7 +28,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               <Countup start={0} end={recovered.value} duration={2.5} separator="," />
             </Typography>
             <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of active case of COVID-19</Typography>
+            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
